821203a50a3921f335f2b648a99c83db356d0433
Huy Đỗ <100893585+dolehuy00@users.noreply.github.com>
2025-06-09 11:42:18 +0700

    add button copy for each message chat bot

--- src/pages/chat/components/ButtonCopyMessage.tsx
+++ src/pages/chat/components/ButtonCopyMessage.tsx
@@ -1,9 +1,24 @@
 import { ActionIcon } from '@mantine/core';
 import { IconCopy, IconCheck } from '@tabler/icons-react';
-import { useEffect, useState } from 'react';
+import { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
 
-export default function ButtonCopyMessage({ value }: { value: string }) {
+
+async function copyPlainMarkdown(markdown: string) {
+    const cleaned = markdown
+        .replace(/\*\*(.*?)\*\*/g, "$1")    // bỏ **bold**
+        .replace(/\*(.*?)\*/g, "$1")        // bỏ *italic*
+        .replace(/\\_/g, "_")              // bỏ escape _
+
+    await navigator.clipboard.writeText(cleaned);
+}
+
+const ButtonCopyMessage = forwardRef(({ value }: { value: string }, ref) => {
     const [copied, setCopied] = useState(false);;
+    const [visible, setCurrentVisible] = useState(false);
+
+    useImperativeHandle(ref, () => ({
+        setVisible: (visible: boolean) => setCurrentVisible(visible),
+    }));
 
     useEffect(() => {
         if (!copied) { return }
@@ -13,23 +28,27 @@ export default function ButtonCopyMessage({ value }: { value: string }) {
 
     return (
         <ActionIcon
+            size="md"
             color={copied ? 'teal' : 'gray'}
             variant="subtle"
             onClick={async () => {
                 copyPlainMarkdown(value);;
                 setCopied(true);
             }}
+            style={{
+                display: visible ? "" : "none",
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                borderBottomRightRadius: 0,
+                borderTopLeftRadius: 0
+            }}
         >
             {copied ? <IconCheck size={16} /> : <IconCopy size={16} />}
         </ActionIcon>
     );
-}
+});
 
-async function copyPlainMarkdown(markdown: string) {
-    const cleaned = markdown
-        .replace(/\*\*(.*?)\*\*/g, "$1") // bỏ **bold**
-        .replace(/\*(.*?)\*/g, "$1")     // bỏ *italic*
-        .replace(/\\_/g, "_");           // bỏ escape _
+ButtonCopyMessage.displayName = 'ButtonCopyMessage';
+export default ButtonCopyMessage;
 
-    await navigator.clipboard.writeText(cleaned);
-}
--- src/pages/chat/components/ChatBotMessageBox.tsx
+++ src/pages/chat/components/ChatBotMessageBox.tsx
@@ -5,19 +5,19 @@ import { LoadChatBotLocalSession } from "../store/ChatBotLocalStore";
 import ButtonCopyMessage from "./ButtonCopyMessage";
 
 const ChatBotMessageBox = forwardRef((_props, ref) => {
+    // state
     const [isLoading, setLoading] = useState(false);
     const [reloadChatBox, setReloadChatBox] = useState(false);
+    // ref
     const scrollRef = useRef<HTMLDivElement>(null);
-    const messages = LoadChatBotLocalSession().message;
+    const copyButtonRefs = useRef<any[]>([]);
+    // other
     const { colorScheme } = useMantineColorScheme();
-    
+    const messages = LoadChatBotLocalSession().message;
+
     useImperativeHandle(ref, () => ({
-        updateLoading: (loading: boolean) => {
-            setLoading(loading);
-        },
-        reloadChatBox: () => {
-            setReloadChatBox(!reloadChatBox)
-        }
+        updateLoading: (loading: boolean) => setLoading(loading),
+        reloadChatBox: () => setReloadChatBox(!reloadChatBox)
     }));
 
     useEffect(() => {
@@ -26,32 +26,38 @@ const ChatBotMessageBox = forwardRef((_props, ref) => {
 
 
     return (
-        <Stack display="flex">
+        <Stack display="flex" gap="xl">
             {messages.map((msg, i) => (
                 <Group
                     key={i}
                     justify={msg.sender === 'user' ? 'flex-end' : 'flex-start'}
                 >
                     <Paper
+                        radius="md"
                         shadow="xs"
-                        p="md"
+                        p="lg"
                         withBorder
                         maw="75%"
                         bg={colorScheme === 'dark'
                             ? msg.sender === 'user' ? '#323232d9' : '#303339a3'
                             : ''
                         }
+                        style={{
+                            position: 'relative',
+                        }}
+                        onMouseEnter={() => copyButtonRefs.current[i]?.setVisible(true)}
+                        onMouseLeave={() => copyButtonRefs.current[i]?.setVisible(false)}
                     >
-                        <ButtonCopyMessage value={msg.message} />
+                        <ButtonCopyMessage key={i} value={msg.message} ref={(el) => (copyButtonRefs.current[i] = el)}/>
                         <TypographyStylesProvider>
                             <ReactMarkdown
                                 components={{
                                     p: ({ children }) => <div>{children}</div>,
-                                    ol: ({ children }) => <ul style={{ paddingLeft: "2rem", marginTop: 2, marginBottom: 2 }}>{children}</ul>,
+                                    ol: ({ children }) => <ol style={{ paddingLeft: "2rem", marginTop: 2, marginBottom: 2 }}>{children}</ol>,
                                     li: ({ children }) => <li style={{ marginBottom: 4 }}>{children}</li>,
                                 }}
                             >
-                                {msg.message}
+                                {msg.message.replace(/\n/g, "  \n")}
                             </ReactMarkdown>
                         </TypographyStylesProvider>
                     </Paper>
